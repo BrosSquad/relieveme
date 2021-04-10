fe00186f9542c78fdc5c715edef7ba59f8df8a9e
CerealKiller97 <bogdanovicstefan1997@gmail.com>
2021-04-10 14:05:21 +0200

    Checkpoint crud done

--- relieveme-back/routes/api.php
+++ relieveme-back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CheckpointsController;
 use App\Http\Controllers\QRCodeGeneratorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/generateQR', [QRCodeGeneratorController::class, 'generate']);
+Route::apiResource('checkpoints', CheckpointsController::class);
