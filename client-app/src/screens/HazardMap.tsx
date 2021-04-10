d3a37a59cd0fc0eaccec0574fd65f3748eac7f1e
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-04-10 22:44:09 +0200

    Add report blockade feature to HazardMap screen

--- client-app/src/screens/HazardMap.tsx
+++ client-app/src/screens/HazardMap.tsx
@@ -9,11 +9,20 @@ import {
   View,
 } from 'react-native'
 import MapView from 'react-native-maps'
+import BottomSheet from 'reanimated-bottom-sheet'
+import ReportBlockadeIcon from '../../assets/report-blockade.svg'
+import BlockadeForm from '../components/BlockadeForm'
+import FAB from '../components/FAB'
+import FABContainer from '../components/FABContainer'
 import MapMarkers from '../components/MapMarkers'
 import { colors } from '../theme'
 
+const SHEET_HEIGHT = 600
+
 const HazardMap: React.FC = () => {
   const [location, setLocation] = React.useState<Location.LocationObject>()
+  const [isSheetOpen, setSheetOpen] = React.useState(false)
+  const sheetRef = React.useRef<BottomSheet>(null)
 
   const watchUserLocation = async () => {
     await Location.watchPositionAsync(
@@ -24,13 +33,39 @@ const HazardMap: React.FC = () => {
     )
   }
 
+  const handleBlockadeSubmit = (description: string) => {
+    console.log(description)
+    setSheetOpen(false)
+  }
+
   React.useEffect(() => {
     watchUserLocation()
   }, [])
 
+  React.useEffect(() => {
+    if (sheetRef.current) {
+      isSheetOpen ? sheetRef.current.snapTo(0) : sheetRef.current.snapTo(1)
+    }
+  }, [isSheetOpen])
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
+      {!isSheetOpen && (
+        <FABContainer position="bottom-right">
+          <FAB color="orange" onPress={() => setSheetOpen(true)}>
+            <ReportBlockadeIcon />
+          </FAB>
+        </FABContainer>
+      )}
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[SHEET_HEIGHT, 0]}
+        borderRadius={32}
+        renderContent={() => <BlockadeForm onSubmit={handleBlockadeSubmit} />}
+        onCloseEnd={() => setSheetOpen(false)}
+      />
+
       {!location ? (
         <ActivityIndicator color={colors.black} />
       ) : (
