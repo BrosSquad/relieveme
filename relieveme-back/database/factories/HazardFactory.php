44d7323618c8735343f9d626efe927ba1ac27168
Dusan Malusev <dmalusev@nanointeractive.com>
2021-04-10 10:19:56 +0200

    Merging with master
    Signed-off-by: Dusan Malusev <dmalusev@nanointeractive.com>

--- relieveme-back/database/factories/HazardFactory.php
+++ relieveme-back/database/factories/HazardFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\Hazard;
+use MStaack\LaravelPostgis\Geometries\Point;
+use MStaack\LaravelPostgis\Geometries\LineString;
+use MStaack\LaravelPostgis\Geometries\Polygon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HazardFactory extends Factory
@@ -21,11 +24,37 @@ class HazardFactory extends Factory
      */
     public function definition()
     {
+        $locations = [
+            new Polygon(
+                [
+                    new LineString(
+                        [
+                            new Point(44.899920, 21.406378),
+                            new Point(44.904548, 21.421913),
+                            new Point(44.892335, 21.436772),
+                            new Point(44.889036, 21.410776),
+                        ]
+                    )
+                ]
+            ),
+            new Polygon(
+                [
+                    new LineString(
+                        [
+                            new Point(44.648979, 20.179532),
+                            new Point(44.674314, 21.421913),
+                            new Point(44.668187, 20.268635),
+                        ]
+                    )
+                ]
+            )
+        ];
+
         return [
             'radius_numbers' => $this->faker->numberBetween(500, 1000),
             'danger' => $this->faker->randomElement(['poplava', 'zemljotres']),
             'level' => $this->faker->numberBetween(1, 5),
-            'location' => $this->faker->randomDigit
+            'location' => $this->faker->randomElement($locations)
         ];
     }
 }
---
+++ relieveme-back/database/factories/UserLocationHistoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserLocationHistory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserLocationHistoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserLocationHistory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
