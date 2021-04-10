164a3973dd250fb37e11b11d2f3ec1ef1522c485
CerealKiller97 <bogdanovicstefan1997@gmail.com>
2021-04-10 17:37:19 +0200

    Map service refactored, map data added to Redis

--- relieveme-back/app/Http/Controllers/MapController.php
+++ relieveme-back/app/Http/Controllers/MapController.php
@@ -2,34 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Blocade;
-use App\Models\Checkpoint;
-use App\Models\Hazard;
-use App\Models\Transport;
+use App\Services\MapService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 class MapController extends Controller
 {
     /**
+     * MapController constructor.
+     *
+     * @param MapService $mapService
+     */
+    public function __construct(private MapService $mapService) {}
+
+    /**
+     * Returns map data
+     *
      * @param int $hazard_id
      *
      * @return JsonResponse
      */
     public function search(int $hazard_id): JsonResponse
     {
-        $hazard = Hazard::whereId($hazard_id)->firstOrFail();
-        $hazard->location->geog
-        $transports = Transport::query();
-        $checkpoints = Checkpoint::query();
-        $blocades = Blocade::whereHazardId($hazard_id)->firstOrFail();
-        $response = [
-            'hazard' => $hazard,
-            'transports' => $transports,
-            'checkpoints' => $checkpoints,
-            'blocades' => $blocades
-        ];
-
-        return response()->json($response, Response::HTTP_OK);
+        try {
+            return response()->json(
+                $this->mapService->getMapData($hazard_id),
+                Response::HTTP_OK
+            );
+        } catch (ModelNotFoundException $exception) {
+            \Log::error($exception->getMessage());
+            $model = explode('\\', $exception->getModel())[2];
+            return \response()->json([
+                'message' => "{$model} not found."
+            ], Response::HTTP_NOT_FOUND);
+        } catch (\Exception $exception) {
+            \Log::error($exception->getMessage());
+            return \response()->json([
+                'message' => $exception->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
