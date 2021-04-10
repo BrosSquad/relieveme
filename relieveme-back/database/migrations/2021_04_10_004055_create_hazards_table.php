4f3b83006e69cd2f837cbf5df8a4d2e0f2b69750
CerealKiller97 <bogdanovicstefan1997@gmail.com>
2021-04-10 02:57:45 +0200

    Hazzards table partial done | suggestions table done

---
+++ relieveme-back/database/migrations/2021_04_10_004055_create_hazards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHazardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hazards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('radius_numbers');
+            $table->integer('danger');
+            $table->integer('level');
+            //$table->geometry('geolocation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hazards');
+    }
+}
