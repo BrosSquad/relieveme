9f39de06928d5914f3687fe8e6fe5c1127fc74c6
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-04-11 09:20:22 +0200

    Add GoogleAPIKey

--- client-app/App.tsx
+++ client-app/App.tsx
@@ -1,3 +1,4 @@
+import * as Location from 'expo-location'
 import React from 'react'
 import 'react-native-gesture-handler'
 import { enableScreens } from 'react-native-screens'
@@ -5,6 +6,8 @@ import RootNavigator from './src/AppNavigator'
 import { NotificationProvider } from './src/hooks/useNotification'
 
 enableScreens()
+
+Location.setGoogleApiKey('AIzaSyB7EReKu7AC70emJ6y-lZGTXcj-_fq1kw4')
 
 export default function App() {
   return (
